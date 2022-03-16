855534b88d556a7fd0fb5a50d460e35ad3bf784f
Paulo Feitosa <paulo@serrabits.com.br>
2022-03-16 15:35:46 -0300

    test: ensure Login start inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,6 +8,12 @@ describe('Login component', () => {
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
-    expect(submitButton.disabled).toBe(true)
+    expect(submitButton.disabled).toBeTruthy()
+    const emailStatus = getByTestId('email-status') as HTMLButtonElement
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    const passwordStatus = getByTestId('password-status') as HTMLButtonElement
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
